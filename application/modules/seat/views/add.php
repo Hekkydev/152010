f74176e58a9ca253d60f1aeccc3962d341c34d85
Hekkydev <nhekky@gmail.com>
2017-04-09 04:28:45 +0700

    created insert seat

--- application/modules/seat/views/add.php
+++ application/modules/seat/views/add.php
@@ -14,26 +14,26 @@ echo form_open('seat/insert');
         <div class="form-group">
             <label class="control-label col-lg-4">Type Kelas :</label>
             <div class="col-lg-6">
-                <input type="text" name="" value="" class="form-control">
+                <input type="text" name="tipe_kelas" value="" class="form-control">
             </div>
         </div>
         <div class="form-group">
             <label class="control-label col-lg-4">Jumlah Seat :</label>
             <div class="col-lg-6">
-                <input type="text" name="" value="" class="form-control">
+                <input type="text" name="jumlah_seat" value="" class="form-control">
             </div>
         </div>
         <div class="form-group">
             <label class="control-label col-lg-4">Jumlah Block :</label>
             <div class="col-lg-6">
-                <input type="text" name="" value="" class="form-control">
+                <input type="text" name="jumlah_block" value="" class="form-control">
             </div>
         </div>
         
         <div class="form-group">
             <label class="control-label col-lg-4">Type Layout :</label>
             <div class="col-lg-6">
-                <select class="form-control input-sm">
+                <select class="form-control input-sm" name="tipe_layout">
                     <option value="col-lg-3">KOLOM 3</option>
                     <option value="col-lg-2">KOLOM 4</option>
                 </select>
